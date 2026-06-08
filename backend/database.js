bf136a056f2156b43f55e65f94b988038433c38f
Elma456 <freshmielma@gmail.com>
2026-06-08 12:33:18 +0530

    Modified Add Teacher Function

--- backend/database.js
+++ backend/database.js
@@ -51,12 +51,12 @@ const readTeacherInfo = async (id) => {
 }
 
 const addTeacher = async (id, name, age) => {
-    const sql = `SELECT * FROM dummyData`
+    const sql = `INSERT INTO teacher(id,name,age) values (?, ?, ?)`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql)
+            .raw(sql, [id, name, age])
             .then((data) => {
-                resolve(data);
+                resolve({status: "Successfully inserted Teacher"});
             })
             .catch((error) => {
                 reject(error);
