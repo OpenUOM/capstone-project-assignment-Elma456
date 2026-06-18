6257a727de5f3ecb9884a73e76fef613932cf6a3
Elma456 <freshmielma@gmail.com>
2026-06-18 07:15:23 +0530

    Modified package.json and server.js

--- frontend/src/app/components/add-new-teacher/add-new-teacher.component.spec.ts
+++ frontend/src/app/components/add-new-teacher/add-new-teacher.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AddNewTeacherComponent } from './add-new-teacher.component';
 
@@ -6,7 +6,7 @@ describe('AddNewTeacherComponent', () => {
   let component: AddNewTeacherComponent;
   let fixture: ComponentFixture<AddNewTeacherComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AddNewTeacherComponent ]
     })
--- frontend/src/app/components/teacher-table/teacher-table.component.spec.ts
+++ frontend/src/app/components/teacher-table/teacher-table.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TeacherTableComponent } from './teacher-table.component';
 
@@ -6,7 +6,7 @@ describe('TeacherTableComponent', () => {
   let component: TeacherTableComponent;
   let fixture: ComponentFixture<TeacherTableComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TeacherTableComponent ]
     })
