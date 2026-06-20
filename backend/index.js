c0ac82d5fe31e79b1a7be41ef64718ce9224f2f8
Elma456 <freshmielma@gmail.com>
2026-06-20 09:13:22 +0530

    Update index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 import server from "./server.js";
 
+// Example of the change applied:
+// Old: const mergedObj = util._extend(obj1, obj2);
+const mergedObj = Object.assign(obj1, obj2); 
 
-if(process.env.NODE_ENV === "test"){
+if (process.env.NODE_ENV === "test") {
   server.listen(4401, function() {
     console.log(
       "Capstone Project Backend is running on http://localhost:4401"
     );
   });
-}else{
+} else {
   server.listen(8080, function() {
     console.log(
       "Capstone Project Backend is running on http://localhost:8080"
