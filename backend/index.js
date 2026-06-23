bdf828395b13a65fe6797016780249330c459e5e
Elma456 <freshmielma@gmail.com>
2026-06-23 16:56:14 +0530

    Change test port from 3000 to 4401

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const server = require("./server.js");
 
-const port = process.env.PORT || (process.env.NODE_ENV === "test" ? 3000 : 8080);
+const port = process.env.PORT || (process.env.NODE_ENV === "test" ? 4401 : 8080);
 
 server.listen(port, () => {
   console.log(`Capstone Project Backend is running on http://localhost:${port}`);
